95cf181569f0f6a06e3ee009b46d899b2c41153c
Marcus-X-Martir <124376409+Marcus-X-Martir@users.noreply.github.com>
2023-02-06 16:24:26 +0000

    building create_fake_csv function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random as r
 from faker import Faker
+import csv
 
 def create_randint_list(k, low=0, high=99):
     return [r.randint(low, high) for i in range(k)]
@@ -7,4 +8,12 @@ def create_randint_list(k, low=0, high=99):
 fake = Faker()
 
 def create_fake_csv(**kwargs):
-    pass
+    fieldnames = []
+    for key, value in kwargs.items():
+        print('{key}: {value}'.format(key=key, value=value))
+        fieldnames.append(key)
+    with open('fake.csv', 'w', newline='') as file:
+        dict_writer = csv.DictWriter(file, delimiter=',', fieldnames=fieldnames)
+        dict_writer.writerow()
+
+create_fake_csv(name='Marcus', age=17)
