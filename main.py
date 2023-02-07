9c0257b2a3fb5c1dc76959ca30e30d45159122d6
Marcus-X-Martir <124376409+Marcus-X-Martir@users.noreply.github.com>
2023-02-07 16:56:20 +0000

    Finished basic create_fake_csv function

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def create_randint_list(k, low=0, high=99):
 
 fake = Faker()
 
+uid = 1
+
 def create_fake_csv(rows, **kwargs):
     """ Pre-Conditions: Takes in keywords and uses the name of the keys as a guide to create a fake csv file
         Post-Condition: A fake csv file is created in the directory that the script is called in
@@ -33,10 +35,26 @@ def create_fake_csv(rows, **kwargs):
                 elif fieldname == 'email':
                     row[fieldname] = fake.email()
                 elif fieldname == 'age':
-                        row[fieldname] = r.randint(kwargs[fieldname][0], kwargs[fieldname][1])
+                    row[fieldname] = r.randint(kwargs[fieldname][0], kwargs[fieldname][1])
+                elif fieldname == 'address':
+                    row[fieldname] = ' '.join(fake.address().split())
+                elif fieldname == 'id':
+                    global uid
+                    row[fieldname] = uid
+                    uid += 1
             dict_writer.writerow(row)
             row = {}
             rows -= 1
 
+#
+# create_fake_csv() can take in the following arguements:
+# REQUIRED: rows = {int value}; This will determine how many rows to write; Take note that an additional row will be added for the header.
+# optional: id = {int value}; The value holds no significance for now,
+#           name = {int value from 1 to 3 inclusive}; 1 = first name; 2 = first and last; 3 = first and last plus possible title,
+#           age = {list with 2 ints in the order of less to greater}; age[0] = minimum age; age[1] = maximum age,
+#           email = {int value}; The value holds no significance for now,
+#           address = {int value}; The value holds no significance for now.
+# 
 
-create_fake_csv(5, name=3, age=[18, 99], email=1, )
+create_fake_csv(5, id=1, name=2, age=[18, 60], email=1, address=1)
+print(create_fake_csv.__doc__)
