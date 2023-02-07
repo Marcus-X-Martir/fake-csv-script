3413db3d611573547ffee4ac8a78da583af29b6c
Marcus-X-Martir <124376409+Marcus-X-Martir@users.noreply.github.com>
2023-02-07 14:59:32 +0000

    create fake csv function almost complete

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def create_randint_list(k, low=0, high=99):
 fake = Faker()
 
 def create_fake_csv(rows, **kwargs):
-    """ Pre-Conditions: Takes in keywords and uses the type(value) of the keys as a guide to create a fake csv file
-        Post-Condition: A fake csv file is created
+    """ Pre-Conditions: Takes in keywords and uses the name of the keys as a guide to create a fake csv file
+        Post-Condition: A fake csv file is created in the directory that the script is called in
     """
     fieldnames = []
     with open('fake.csv', 'w', newline='') as file:
@@ -22,12 +22,21 @@ def create_fake_csv(rows, **kwargs):
         while rows > 0:
             for fieldname in fieldnames:
                 if fieldname == 'name':
-                    row[fieldname] = fake.name()
+                    if kwargs[fieldname] == 1:
+                        row[fieldname] = fake.name().split()[0]
+                    elif kwargs[fieldname] == 2:
+                        row[fieldname] = ' '.join(fake.name().split()[:2])
+                    elif kwargs[fieldname] == 3:
+                        row[fieldname] = ' '.join(fake.name().split()[:3])
+                    else:
+                        raise(valueError)
+                elif fieldname == 'email':
+                    row[fieldname] = fake.email()
                 elif fieldname == 'age':
-                    row[fieldname] = 
+                        row[fieldname] = r.randint(kwargs[fieldname][0], kwargs[fieldname][1])
             dict_writer.writerow(row)
             row = {}
             rows -= 1
 
 
-create_fake_csv(3, name=9)
+create_fake_csv(5, name=3, age=[18, 99], email=1, )
